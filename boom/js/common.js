7c0a73d27958f9785054e9721ab80767e5ded625
Alex02x <36463198+Alex02x@users.noreply.github.com>
2020-08-23 17:52:03 +0700

    Add files via upload

---
+++ boom/js/common.js
@@ -0,0 +1,14 @@
+/* Use SVG Support For Old Browsers */
+svg4everybody();
+
+/* Fully Loaded */
+$(window).on('load', function() {
+
+	/* Decoration Animation Border */
+	setTimeout(function() {
+		$(".needAnimationBorder").addClass("animationBorder");
+	}, 2000)
+
+});
+
+
